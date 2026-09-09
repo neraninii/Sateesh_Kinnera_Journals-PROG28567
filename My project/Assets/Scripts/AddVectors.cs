4a6ed2a0deb1dd865d0c3e769facec18767f84a2
Kinn <neraninii@gmail.com>
2026-09-09 12:42:19 -0400

    added project to repository

---
+++ My project/Assets/Scripts/AddVectors.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AddVectors : MonoBehaviour
+{
+    public Transform redTransform;
+    public Transform blueTransform;
+
+    void Update()
+    {
+        // Exercise: Visualizing Vector Addition
+        // Create a new Vector2 called "rPlusB" which adds rTransform's position to bTransform's position.​
+
+        Vector2 rPlusB = redTransform.position + blueTransform.position; 
+
+        // When the B key is held down, draw a blue line from the origin to bTransform's position.​
+
+        // When the R key is held down, draw a red line from the origin to rTransform's position.​
+
+        // When the R and B keys are both held down, draw a magenta from the origin to rPlusB.​
+
+        if (Keyboard.current.bKey.isPressed && Keyboard.current.rKey.isPressed)
+        {
+            Debug.DrawLine(Vector2.zero, rPlusB, Color.magenta);
+        }
+        else if (Keyboard.current.bKey.isPressed)
+        {
+            Debug.DrawLine(Vector2.zero, blueTransform.position, Color.blue);
+        }
+        else if (Keyboard.current.rKey.isPressed)
+        {
+            Debug.DrawLine(Vector2.zero, redTransform.position, Color.red);
+        }
+
+        
+
+        // Exercise: Calculating Magnitude
+        // Calculate the magnitude of bPlusR using the mathematical formula for calculating the length of a vector – Pythagorean Theroem.​
+
+        float magnitude = Mathf.Sqrt(Mathf.Pow(rPlusB.x, 2) + Mathf.Pow(rPlusB.y, 2));
+
+        // Output the magnitude to the console using either Debug.Log or print.
+        Debug.Log(magnitude);
+    }
+}
---
+++ My project/Assets/Scripts/DrawVectors.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DrawVectors : MonoBehaviour
+{
+    void Update()
+    {
+
+        // Create two Vector2 variables, "dVector" and "eVector"
+        // Set their values to dVector: (0, 1) and eVector: (3, -2)
+
+        Vector2 dVector = new Vector2(0, 1);
+        Vector2 eVector = new Vector2(3, -2);
+
+        Debug.DrawLine(Vector2.zero, dVector, Color.yellow); 
+        Debug.DrawLine(Vector2.zero, eVector, Color.grey);
+
+        // Use Debug.DrawLine to draw a yellow vector starting at the origin and ending at dVector.
+        // Use Debug.DrawLine to draw a grey vector starting at the origin and ending at eVector.
+    }
+}
