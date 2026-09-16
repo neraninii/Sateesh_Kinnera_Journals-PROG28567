1bbcdab9d47965044c7ffbf79e647ed8953776d5
Kinn <neraninii@gmail.com>
2026-09-16 10:40:42 -0400

    Week 2 exercise - instantiated bomb above the player

---
+++ Assets/Scripts/Week 2/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            bombOffset(Vector3.up);
+        }
+
+    }
+
+    void bombOffset(Vector3 offset)
+    {
+        Instantiate(bombPrefab, transform.position + offset, Quaternion.identity);
+    }
+}
