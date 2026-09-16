b392aba98e7bd089d59b874d70702a391cceb374
Kinn <neraninii@gmail.com>
2026-09-16 11:37:42 -0400

    Adding comments

--- Assets/Scripts/Week 2/Player.cs
+++ Assets/Scripts/Week 2/Player.cs
@@ -12,11 +12,14 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+        // Checking if bomb spawns above player
         if (Keyboard.current.bKey.wasPressedThisFrame)
         {
             bombOffset(Vector3.up);
         }
 
+        
+        // Checking if points are normalized
         if (Keyboard.current.pKey.wasPressedThisFrame)
         {
             Debug.Log(normalize(new Vector2(3,4)));
@@ -26,16 +29,23 @@ public class Player : MonoBehaviour
 
     }
 
+    // Spawning a bomb above the player using methods and offsets
     void bombOffset(Vector3 offset)
     {
         Instantiate(bombPrefab, transform.position + offset, Quaternion.identity);
     }
 
+    // Normalizing points using the formula
     Vector2 normalize(Vector2 inVector)
     {
         float magnitude = Mathf.Sqrt(Mathf.Pow(inVector.x, 2) + Mathf.Pow(inVector.y, 2));
         Vector2 outVector = new Vector2(inVector.x / magnitude, inVector.y / magnitude);
 
         return outVector;
+    }
+
+    void warpPlayer(Transform target, float ratio)
+    {
+        
     }
 }
